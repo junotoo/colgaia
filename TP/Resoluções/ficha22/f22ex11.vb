9781d1a1f7fb54e10472128d117a4e227d9d94ee
Creme <mouratiago082@gmail.com>
2022-02-04 08:30:28 +0000

    TP - tiago

---
+++ TP/Resoluções/ficha22/f22ex11.vb
@@ -0,0 +1,32 @@
+﻿Module f22ex11
+    Sub Main(args As String())
+
+        Dim num As Integer
+        Dim i As Integer
+        Dim max As Integer
+        Dim min As Integer
+
+        i = 0
+        max = -32767
+        min = 32768
+        Do
+            num = pedirInteger()
+            i = i + 1
+            If num > max Then
+                max = num
+            ElseIf num < min Then
+                min = num
+            End If
+            Console.Clear()
+
+        Loop Until i = 3
+
+        Console.WriteLine("O maior lido foi {0} e o menor valor lido foi {1}.", max, min)
+        Console.WriteLine("Prima uma tecla para continuar...")
+        Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
