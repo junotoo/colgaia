9e05a93f4137313a223ceb6e6b163e5d88f67b1a
junotoo <31505973@colgaia.pt>
2022-02-04 08:43:26 +0000

    TP - Juno

--- TP/Resoluções/ficha22/f22ex8.vb
+++ TP/Resoluções/ficha22/f22ex8.vb
@@ -1,15 +1,11 @@
 ﻿Module f22ex8
     Sub Main(args As String())
-        Dim num, pares, impares As Integer
+        Dim num As Integer
         Dim c As Integer = 0
         Do
-            c += 1
             num = pedirInteger()
-            Console.Clear()
-            pares += numsPares(num)
-        Loop Until c = 30
-        impares = 30 - pares
-        Console.WriteLine("Dos {0} números lidos, {1} eram pares e {2} eram ímpares.", c, pares, impares)
-        esperaAlt()
+            c += 1
+        Loop Until num = 0
+        Console.WriteLine("Foram lidos {0} valores.", c)
     End Sub
 End Module
--- TP/Resoluções/ficha22/f22ex9.vb
+++ TP/Resoluções/ficha22/f22ex9.vb
@@ -1,4 +1,15 @@
 ﻿Module f22ex9
     Sub Main(args As String())
-
+        Dim num, pares, impares As Integer
+        Dim c As Integer = 0
+        Do
+            c += 1
+            num = pedirInteger()
+            Console.Clear()
+            pares += numsPares(num)
+        Loop Until c = 30
+        impares = 30 - pares
+        Console.WriteLine("Dos {0} números lidos, {1} eram pares e {2} eram ímpares.", c, pares, impares)
+        esperaAlt()
+    End Sub
 End Module
