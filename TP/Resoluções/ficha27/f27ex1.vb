073ff10de794f05ed46d0397c04978189e5e8afe
junotoo <31505973@colgaia.pt>
2022-02-17 16:00:34 +0000

    Fix ficha 27

--- TP/Resoluções/ficha27/f27ex1.vb
+++ TP/Resoluções/ficha27/f27ex1.vb
@@ -1,20 +1,16 @@
 ﻿Module f27ex1
     Sub Main(args As String())
-        Const senha = "senha"
+        Dim senha = "senha"
         Dim insertpass As String
-
-
         Do
-            Console.WriteLine("Insira a sua password:_")
-            insertpass = Console.ReadLine()
-            If insertpass = "senha" Then
+            insertpass = pedirString("Insira a sua palavra passe: ")
+            If insertpass = senha Then
                 Console.WriteLine("Utilizador Autorizado!!!")
             Else
                 Console.WriteLine("Utilizador Bloqueado!!!")
             End If
             esperaAlt()
-
-        Loop Until insertpass = "senha"
+        Loop Until insertpass = senha
         esperaAlt()
 
     End Sub
