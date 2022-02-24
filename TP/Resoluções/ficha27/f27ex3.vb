ce54df2f8767863f63cb4938dfef947219f2e1bc
junotoo <31505973@colgaia.pt>
2022-02-24 16:41:51 +0000

    TP

--- TP/Resoluções/ficha27/f27ex3.vb
+++ TP/Resoluções/ficha27/f27ex3.vb
@@ -4,7 +4,11 @@
         Dim seq2 As String = 0
 
         Console.OutputEncoding = System.Text.Encoding.UTF8
+        seq = pedirString("Introduza uma sequência de carateres: ")
 
+        Console.WriteLine("")
+        Console.WriteLine("")
+        Console.WriteLine("")
 
         seq2 = seq.Replace("1", "*").Replace("2", "*").Replace("3", "*").Replace("4", "*").Replace("4", "*").Replace("5", "*").Replace("6", "*").Replace("7", "*").Replace("8", "*").Replace("9", "*")
         Console.WriteLine(seq2)
